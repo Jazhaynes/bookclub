359339b3e2e1bc5f5f9fb4190a1fdcb6bd03272d
Jasmine Haynes <jashaynes@my365.bellevue.edu>
2024-03-16 22:34:17 -0700

    initial commit.

---
+++ src/main/java/com/bookclub/BookclubHaynesApplication.java
@@ -0,0 +1,13 @@
+package com.bookclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookclubHaynesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookclubHaynesApplication.class, args);
+	}
+
+}
